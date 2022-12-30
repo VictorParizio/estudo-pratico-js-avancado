c3b2705e4b893055dfa004823ddc800d0678a806
Victor Parizio <victorparizio@hotmail.com>
2022-12-30 17:46:15 -0300

    Desestruturação JavaScript

---
+++ 05-desestrururacao-de-objetos-e-arrays/index.js
@@ -0,0 +1,19 @@
+let pessoa = { nome: 'Roberto', sobrenome: 'Dias', idade: 33 }
+/*
+let nome = pessoa.nome
+let idade = pessoa.idade
+
+let { nome: nome, idade: idade, sobrenome: sobrenome } = pessoa
+let { nome, idade, sobrenome } = pessoa
+
+console.log(nome)
+console.log(idade)
+console.log(sobrenome)
+*/
+
+const numeros = [ 1, 2, 3]
+
+const [ um, dois, três] = numeros
+
+console.log(um)
+console.log(dois)
