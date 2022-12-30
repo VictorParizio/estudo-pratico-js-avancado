103cff0287f768afdc9ea82f259f233c3912c50b
Victor Parizio <victorparizio@hotmail.com>
2022-12-30 19:46:58 -0300

    Promises

---
+++ 10-promises/index.js
@@ -0,0 +1,28 @@
+/*
+    Pendente
+    Realizado Resolve
+    Recusada Reject
+    Estabelecida
+*/
+/*
+new Promise((resolve, reject) => {
+
+})
+*/
+
+let ferverAgua = (chaleiraEstaNoFogao, fogaoEstaLigado) => {
+    return new Promise((resolve, reject) => {
+        if (chaleiraEstaNoFogao && fogaoEstaLigado) {
+            console.log('começando o processo de ferver água')
+            resolve()
+        } else {
+            console.log('é necessário ligar o fogão e colocar a chaleira para ferver a água')
+            reject()
+        }
+    })
+}
+
+let chaleiraEstaNoFogao = true
+let fogaoEstaLigado = true
+ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado)
+console.log('fazendo café')
